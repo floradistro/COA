c2408e509344bf5dcff2c88d4d195a2f70f0b99a
floradistro <fahad@cwscommercial.com>
2025-12-28 03:36:35 -0500

    Add allowedDevOrigins for dev.wh4le.net proxy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['https://dev.wh4le.net'],
   webpack: (config) => {
     // Handle PDF.js worker
     config.resolve.alias = {
